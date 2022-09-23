65913bec4025da8734f602acd932cd735fdfa613
Vitalik Buterin <VitalikZhou@gmail.com>
2022-09-23 08:18:02 +0800

    types

--- types/function.ts
+++ types/function.ts
@@ -88,15 +88,15 @@
 /**
  * 函数重载
  */
- function reverse(x: number): number;
- function reverse(x: string): string;
- function reverse(x: number | string): number | string | void {
-     if (typeof x === 'number') {
-         return Number(x.toString().split('').reverse().join(''));
-     } else if (typeof x === 'string') {
-         return x.split('').reverse().join('');
-     }
- }
+//  function reverse(x: number): number;
+//  function reverse(x: string): string;
+//  function reverse(x: number | string): number | string | void {
+//      if (typeof x === 'number') {
+//          return Number(x.toString().split('').reverse().join(''));
+//      } else if (typeof x === 'string') {
+//          return x.split('').reverse().join('');
+//      }
+//  }
 
- console.log(reverse(9527))
- console.log(reverse("9527"))
+//  console.log(reverse(9527))
+//  console.log(reverse("9527"))
